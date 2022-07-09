c799fec8ed8798aee0927c62e54d0a9958888032
Benjamin Glitsos <bengyup@gmail.com>
2022-07-09 13:27:08 +1000

    Reading from XML config in Linux home directory working

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-console.log("Hello world");
+const fs = require("fs");
+const parseXml = require("xml-js");
+const axios = require("axios");
+
+fs.readFile(`${process.env.HOME}/.stock-alerts.xml`, "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
