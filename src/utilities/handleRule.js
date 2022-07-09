8f39be90235abf88c0e67ebf84061e36eea2abe3
Benjamin Glitsos <bengyup@gmail.com>
2022-07-09 15:08:50 +1000

    Handler now returning result properly

--- src/utilities/handleRule.js
+++ src/utilities/handleRule.js
@@ -1,6 +1,7 @@
 module.exports = async (scriptFilename, settings, parameters) => {
     try {
-        return await console.log(require(`../rules/${scriptFilename}`)(settings, parameters));
+        const result = await require(`../rules/${scriptFilename}`)(settings, parameters);
+        console.log(result);
     } catch(err) {
         console.error(err);
     }
