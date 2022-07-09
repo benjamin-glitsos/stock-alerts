3651316639458e1cf57b5383cc6ffca1dd167bd7
Benjamin Glitsos <bengyup@gmail.com>
2022-07-09 16:53:47 +1000

    Added templating to custom messages

--- src/utilities/handleRule.js
+++ src/utilities/handleRule.js
@@ -1,13 +1,15 @@
 const handleError = require("./handleError");
+const Mustache = require("mustache");
 
 module.exports = async (scriptFilename, settings, parameters) => {
     try {
         const result = await require(`../rules/${scriptFilename}`)(settings, parameters);
         if (result.triggered) {
+            const messageTemplated = parameters.message ? Mustache.render(parameters.message, parameters) : result.message;
             console.log({ 
                 id: result.id,
                 symbol: result.symbol,
-                message: parameters.message || result.message
+                message: messageTemplated
             });
         }
     } catch(err) {
