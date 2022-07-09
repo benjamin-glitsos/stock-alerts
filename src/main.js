d25132873f48a55560f03e8dccdb380a657082f2
Benjamin Glitsos <bengyup@gmail.com>
2022-07-09 14:11:03 +1000

    Using async/await monad instead of callbacks

--- src/main.js
+++ src/main.js
@@ -1,21 +1,29 @@
 const fs = require("fs");
+const fsPromises = fs.promises;
 const parseXml = require("xml-js");
 const axios = require("axios");
 
-fs.readFile(`${process.env.HOME}/.stock-alerts.xml`, "utf8", (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
+(async () => {
+    try {
+        const configXml = await fsPromises.readFile(
+            `${process.env.HOME}/.stock-alerts.xml`,
+            "utf8"
+        );
+        const config = parseXml.xml2js(configXml, { compact: true });
+        const configSettings = config.Rules._attributes;
+        const configRules = config.Rules;
 
-  const config = parseXml.xml2js(data, { compact: true });
-  const globalConfig = config.Rules._attributes;
-  axios
-    .get(`https://yfapi.net/v11/finance/quoteSummary/AAPL?modules=summaryDetail`, {
-      headers: {
-        "x-api-key": globalConfig.yahooFinanceApiKey,
-      },
-    })
-    .then((res) => console.log(res.data.quoteSummary.result[0].summaryDetail.previousClose.raw))
-    .catch((error) => console.error(error));
-});
+        const res = await axios.get(
+            `https://yfapi.net/v11/finance/quoteSummary/AAPL?modules=summaryDetail`,
+            {
+                headers: {
+                    "x-api-key": configSettings.yahooFinanceApiKey,
+                },
+            }
+        );
+
+        console.log(res.data.quoteSummary.result[0].summaryDetail.previousClose.raw);
+    } catch(err) {
+        console.error(err);
+    }
+})();
