4cc3f8b7c0c04f0371a8906797ca2312f1ec5ad9
Benjamin Glitsos <bengyup@gmail.com>
2022-07-13 20:31:22 +1000

    Commenting out in config XML file now working

--- src/main.js
+++ src/main.js
@@ -10,12 +10,14 @@ import priceMinimum from "./ruleTypes/priceMinimum.js";
         .catch(err =>
             console.error(`Could not open config file: ${configFilepath}`)
         )
-        .then(data => parseXml.xml2js(data, { compact: true }));
+        .then(data =>
+            parseXml.xml2js(data, { compact: true, ignoreComment: true })
+        );
     const configSettings = config.Rules._attributes;
     const configRules = config.Rules;
+    console.log(config);
 
     // TODO: rules should be: Reminder, PriceLimit, PriceChange, ProfitLimit
-    // TODO: make sure commenting out rules is working
     // TODO: add currency conversion using yahoo finance api. put this inside every ruleType
     // TODO: load xml on each run; this allows it to 'hot load' the config
     // TODO: don't use' cron, instead, put everything on a one day loop. And have an 'every' field to throttle it using local memory - takes a number (this will be number of days). The throttle stores into an object in memory with the rule IDs as keys and dates as values
