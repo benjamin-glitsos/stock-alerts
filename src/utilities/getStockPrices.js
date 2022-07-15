791e9400dc62182e06b9adc08d4308f059784a09
Benjamin Glitsos <bengyup@gmail.com>
2022-07-16 08:01:53 +1000

    Made API error log the message from API response, and improved API error handling

--- src/utilities/getStockPrices.js
+++ src/utilities/getStockPrices.js
@@ -16,6 +16,16 @@ export default async ({ apiKey, id, symbol, range }) => {
                 `https://yfapi.net/v8/finance/chart/${symbol}?range=${range}&interval=1d`,
                 headers
             )
+            .catch(error => {
+                const maybeMessage = error.response.data.message;
+                const errorMessage = `Could not fetch '${symbol}' from Yahoo Finance.`;
+                if (maybeMessage) {
+                    handleError(id, `${errorMessage} Reason: ${maybeMessage}`);
+                } else {
+                    handleError(id, errorMessage);
+                }
+                throw Error(error);
+            })
             .then(res => res.data.chart.result[0].indicators.quote[0].close);
 
         const startPrice = prices[0];
@@ -23,10 +33,5 @@ export default async ({ apiKey, id, symbol, range }) => {
         const priceRange = [startPrice, endPrice].map(roundTwoDecimals);
 
         return priceRange;
-    } catch (err) {
-        handleError(
-            id,
-            `Could not fetch stock prices for '${symbol}' from Yahoo Finance`
-        );
-    }
+    } catch (error) {}
 };
